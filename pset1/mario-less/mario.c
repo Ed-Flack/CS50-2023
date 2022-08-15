d06bcf1ae798f3cc023627d6dd7659d62dd3d82e
Edward Flack <44028291+Ed-Flack@users.noreply.github.com>
2022-08-15 11:50:58 +0100

    Create mario.c

---
+++ pset1/mario-less/mario.c
@@ -0,0 +1,28 @@
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // User inputs they height the want, only takes between 1 and 8.
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    // Builds the first half of the mario 'pyramid'
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < height - i - 1; j++)
+        {
+            printf(" "); // This aligns the bricks
+        }
+        for (int j = 0; j < height - (height - i - 1); j++)
+        {
+            printf("#"); // This is used to repersent the bricks
+        }
+        printf("\n");
+    }
+}

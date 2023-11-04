54373ace4182a15f9d6848677dd0d46dd6278dd5
Edward Flack <44028291+Ed-Flack@users.noreply.github.com>
2023-11-04 21:42:10 +0000

    Create cash.py

---
+++ week6/pset6/sentimental-cash/cash.py
@@ -0,0 +1,19 @@
+import cs50 as cs50
+
+def getCoins(change, coinValue):
+    coins = 0
+    while (change - coinValue >= 0):
+        change -= coinValue
+        coins += 1
+    return round(change, 2), coins
+
+change = -1
+while (change < 0):
+    change = cs50.get_float("Change owed: ")
+
+change, quarters = getCoins(change, 0.25)
+change, dimes = getCoins(change, 0.1)
+change, nickels = getCoins(change, 0.05)
+change, pennies = getCoins(change, 0.01)
+
+print(quarters + dimes + nickels + pennies)

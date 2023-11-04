7799a02ce32be9917d18b2fc69bf41e1a65d66f0
Edward Flack <44028291+Ed-Flack@users.noreply.github.com>
2023-11-04 14:57:04 +0000

    Create mario.py

---
+++ week6/pset6/sentimental-mario-less/mario.py
@@ -0,0 +1,11 @@
+# TODO
+height = ""
+while not height.isnumeric() or int(height) < 1 or int(height) > 8:
+    height = input("Height: ")
+height = int(height)
+for i in range(height):
+    for j in range(height - i - 1):
+        print(" ", end="")
+    for j in range(height - (height - i - 1)):
+        print("#", end="")
+    print()

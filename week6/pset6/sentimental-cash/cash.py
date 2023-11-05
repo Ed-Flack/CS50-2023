39ccd7758051893189fe9ffdff6b87f59204c0d9
Edward Flack <44028291+Ed-Flack@users.noreply.github.com>
2023-11-05 13:38:24 +0000

    Update cash.py

--- week6/pset6/sentimental-cash/cash.py
+++ week6/pset6/sentimental-cash/cash.py
@@ -1,14 +1,16 @@
-import cs50
+import cs50 as cs50
+
 
 def getCoins(change, coinValue):
     coins = 0
-    while (change - coinValue >= 0):
+    while change - coinValue >= 0:
         change -= coinValue
         coins += 1
     return round(change, 2), coins
 
+
 change = -1
-while (change < 0):
+while change < 0:
     change = cs50.get_float("Change owed: ")
 
 change, quarters = getCoins(change, 0.25)

83b7f5b2153aa432f310ccd528076cc6627c7556
Edward Flack <44028291+Ed-Flack@users.noreply.github.com>
2023-11-11 16:45:24 +0000

    Update app.py

--- week9/lab9/birthdays/app.py
+++ week9/lab9/birthdays/app.py
@@ -22,21 +22,32 @@ def after_request(response):
     return response
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST", "PUT", "DELETE"])
 def index():
     if request.method == "POST":
-
         # TODO: Add the user's entry into the database
+        print("test")
         name = request.form.get("name")
         month = request.form.get("month")
         day = request.form.get("day")
         db.execute("INSERT INTO birthdays (name, month, day) VALUES (?, ?, ?)", name, month, day)
         return redirect("/")
 
-    else:
+    elif request.method == "PUT":
+        id = request.form.get("id")
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+        db.execute("UPDATE birthdays SET name = ?, month = ?, day = ? WHERE id=?", name, month, day, id)
+        return "saved"
 
+    elif request.method == "DELETE":
+        id = request.form.get("id")
+        db.execute("DELETE FROM birthdays WHERE id=?", id)
+        birthdays = db.execute("SELECT * FROM birthdays")
+        return "deleted"
+
+    else:
         # TODO: Display the entries in the database on index.html
         birthdays = db.execute("SELECT * FROM birthdays")
         return render_template("index.html", birthdays=birthdays)
-
-
